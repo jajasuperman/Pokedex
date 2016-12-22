645c8a247c0b2682b9085ae3ecbcbf859dc0e6f5
Eneko <enekosampedro@gmail.com>
2016-12-22 15:35:04 +0100

    Metodoak ListBoxBean-ean

--- src/main/java/com/pokedex/SelectOneListBoxBean.java
+++ src/main/java/com/pokedex/SelectOneListBoxBean.java
@@ -17,8 +17,12 @@ public class SelectOneListBoxBean {
 
         selectedPokemon = null;
 
+        pokeList = new ArrayList<>();
+
         for (int i = 1; i <= 721; i++) {
-            this.pokeList.add(new Pokemon(i, Deiak.getInstance().getPokeIzena(i)));
+            String izena = Deiak.getInstance().getPokeIzena(i);
+            izena = izena.substring(0, 1).toUpperCase() + izena.substring(1);
+            this.pokeList.add(new Pokemon(i, izena));
         }
     }
 
@@ -49,5 +53,63 @@ public class SelectOneListBoxBean {
                 break;
             }
         }
+        
+        Deiak.getInstance().id = Integer.parseInt(selectedPokemon);
+    }    
+    
+    
+    /* INFO 1 */
+    
+    public String getWeight() {
+        return Integer.toString(Deiak.getInstance().getPokeWeight());
+    }
+    public String getHeight() {
+        return Integer.toString(Deiak.getInstance().getPokeHeight());
+    }
+    public String getGen() {
+        return Deiak.getInstance().getPokeGen();
+    }
+    
+    /* INFO 2 */
+    public String getGender() {
+        return Integer.toString(Deiak.getInstance().getPokeGender());
+    }
+    public String getColour() {
+        return Deiak.getInstance().getPokeColour();
+    }
+    public String getShape() {
+        return Deiak.getInstance().getPokeShape();
+    }
+    public String getHabitat() {
+        return Deiak.getInstance().getPokeHabitat();
+    }
+    
+    /* INFO 3*/
+    public String getCaptureRate() {
+        return Integer.toString(Deiak.getInstance().getPokeCaptureRate());
+    }
+    public String getHappiness() {
+        return Integer.toString(Deiak.getInstance().getPokeHappiness());
+    }
+    public String getBaseExp() {
+        return Integer.toString(Deiak.getInstance().getPokeBaseExp());
+    }
+    
+    /* INFO 4 */
+    public String getEgg() {
+        return Deiak.getInstance().getPokeEgg();
+    }
+    public String getBaby() {
+        int baby = Deiak.getInstance().getPokeBaby();
+        if(baby == 0) {
+            return "No";
+        }
+        return "Yes";
+    }
+    public String getHatch() {
+        return Integer.toString(Deiak.getInstance().getPokeHatch());
+    }
+    public String getGrowRate() {
+        return Deiak.getInstance().getPokeGrowthRate();
     }
 }
